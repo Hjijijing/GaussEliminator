e374c9a127dbf2b5acd0f9ca9f46d854d58afc27
Hjijijing <rashansen69@gmail.com>
2022-12-17 15:18:19 +0100

    Updates

--- gauss.js
+++ gauss.js
@@ -7,12 +7,36 @@ let latexoutput = document.querySelector("#latexoutput");
 
 let queryString = window.location.search;
 
+let buttonQuotes = ["ELIMINATING!", "CHOOSING HIGHLANDER!", "MURDERING ROWS!"];
+let outputQuotes = [
+  "Eliminating...",
+  "Choosing highlander...",
+  "Murderings rows...",
+];
+
+console.log(document.location);
+
+function Christmas() {
+  let date = new Date();
+
+  if (date.getDate() >= 17 && date.getMonth() == 11) {
+    console.log("Merry Christmas!");
+    eliminateButton.textContent = "MERRY CHRISTMAS!";
+    if (date.getDate() == 31) {
+      console.log("And a happy new year!");
+      eliminateButton.textContent = "HAPPY NEW YEAR!";
+    }
+  }
+}
+
+Christmas();
+
 function renderOutput() {
   while (latexoutput.firstChild) {
     latexoutput.removeChild(latexoutput.firstChild);
   }
 
-  katex.render(`${result}`, latexoutput, {
+  katex.render(`${outputField.value}`, latexoutput, {
     throwOnError: false,
     displayMode: true,
   });
@@ -29,7 +53,7 @@ function loadQuery(query) {
   let l = query.split(";");
   let output = "";
   for (let i = 0; i < l.length; i++) {
-    output += l[i].split(":").join(" ");
+    output += l[i].split("&").join(" ");
     if (i != l.length - 1) output += "\n";
   }
 
@@ -78,13 +102,18 @@ let calculating = false;
 let evaluateOutput = false;
 
 let standardOptions = {
-  divideVariables: false,
+  divideVariables: true,
   matrixOpen: " \\left[ ",
   matrixClose: " \\right] ",
   printLink: true,
   spacing: "6pt",
   switch: false,
   inverse: false,
+  equalSign: "\\sim",
+  steps: true,
+  plain: false,
+  help: false,
+  resultOnly: false,
 };
 
 let options = { ...standardOptions };
@@ -96,8 +125,9 @@ function StartElimination() {
   calculating = true;
   eliminateButton.classList.toggle("calculating");
   head.classList.toggle("active");
-  eliminateButton.textContent = "ELIMINATING!";
-  outputField.value = "Eliminating...";
+  let r = Math.floor(Math.random() * buttonQuotes.length);
+  eliminateButton.textContent = buttonQuotes[r];
+  outputField.value = outputQuotes[r];
   return new Promise((res, rej) => {
     setTimeout(() => {
       Eliminate();
@@ -107,6 +137,7 @@ function StartElimination() {
     calculating = false;
     eliminateButton.classList.toggle("calculating");
     eliminateButton.textContent = "ELIMINATE!";
+    Christmas();
     head.classList.toggle("active");
   });
 }
@@ -132,6 +163,11 @@ function Eliminate() {
   }
 }
 
+function help() {
+  options.help = true;
+  outputField.value = availableOptionsText;
+}
+
 function ParseInput() {
   arrayStructure = "";
   options = { ...standardOptions };
@@ -144,16 +180,22 @@ function ParseInput() {
   }
 
   if (
-    lines.length > 1 &&
+    lines.length > 0 &&
     lines[lines.length - 1].toLowerCase().startsWith("opt")
   ) {
     let optionInput = lines[lines.length - 1].split(" ");
     lines.splice(lines.length - 1, 1);
 
+    if (optionInput.length == 1 && lines.length == 0) {
+      help();
+      return;
+    }
+
     for (let i = 1; i < optionInput.length; i++) {
       let option = optionInput[i];
 
-      if (option == "nonzero") options.divideVariables = true;
+      if (option.toLowerCase() == "nonzero") options.divideVariables = true;
+      if (option.toLowerCase() == "zero") options.divideVariables = false;
 
       if (option == "bmatrix") {
         options.matrixOpen = "\\left[";
@@ -180,16 +222,35 @@ function ParseInput() {
         options.matrixClose = "\\right)";
       }
 
-      if (option == "switch") options.switch = true;
+      if (option.toLowerCase() == "switch") options.switch = true;
+      if (option.toLowerCase() == "noswitch") options.switch = false;
 
-      if (option == "link") options.printLink = true;
-      if (option == "nolink") options.printLink = false;
+      if (option.toLowerCase() == "link") options.printLink = true;
+      if (option.toLowerCase() == "nolink") options.printLink = false;
 
-      if (option.startsWith("vspace")) {
+      if (option.toLowerCase() == "nosteps") options.steps = false;
+      if (option.toLowerCase() == "steps") options.steps = true;
+
+      if (option.toLowerCase() == "plain") options.plain = true;
+      if (option.toLowerCase() == "result") {
+        options.resultOnly = true;
+        options.steps = false;
+      }
+
+      if (option.toLowerCase() == "help") {
+        help();
+        return;
+      }
+
+      if (option.toLowerCase().startsWith("vspace")) {
         options.spacing = option.split("=")[1];
       }
 
-      if (option == "inverse") options.inverse = true;
+      if (option.toLowerCase().startsWith("equal")) {
+        options.equalSign = option.split("=")[1];
+      }
+
+      if (option.toLowerCase() == "inverse") options.inverse = true;
     }
   }
 
@@ -197,6 +258,10 @@ function ParseInput() {
     let split = lines[i].split(" ");
     let columns = [];
     for (let j = 0; j < split.length; j++) {
+      if (split[j] == "help") {
+        help();
+        return;
+      }
       if (split[j] == "") continue;
       if (split[j] == "|") {
         if (i == 0) arrayStructure += "|";
@@ -238,28 +303,39 @@ function Main() {
   operations = [];
   fillOperations();
 
-  SaveStatus();
-  Gauss();
-  CompleteOperations();
+  if (options.help == false) {
+    if (options.resultOnly == false) SaveStatus();
+    if (options.plain == false) {
+      Gauss();
+      CompleteOperations(true);
+      if (options.steps == false) SaveStatus(options.resultOnly == false);
+    }
 
-  result +=
-    "\n\\end{align*}" +
-    "%gauss.notrasmus.com" +
-    (options.printLink ? `/?` + getQuery() : "") +
-    (evaluateOutput ? " . Numerical evaluation, might not be exact." : "");
+    let link = document.location.host + document.location.pathname;
+    link = link.substring(0, link.length - 1);
 
-  outputField.value = result;
-  //console.log(result);
+    if (link != "gauss.notrasmus.com") link = "gauss.notrasmus.com " + link;
+
+    result +=
+      "\n\\end{align*}" +
+      "%" +
+      link +
+      (options.printLink ? `/?` + getQuery() : "") +
+      (evaluateOutput ? " . Numerical evaluation, might not be exact." : "");
+
+    outputField.value = result;
+    //console.log(result);
+  }
 
   try {
     renderOutput();
   } catch (e) {}
 
-  try {
-    navigator.clipboard.writeText(result);
-  } catch (e) {
-    //console.error("Could not copy to clipboard");
-  }
+  // try {
+  //   navigator.clipboard.writeText(result);
+  // } catch (e) {
+  //   //console.error("Could not copy to clipboard");
+  // }
 }
 
 function Gauss() {
@@ -330,16 +406,7 @@ function SwapRows(a, b) {
   rows[a] = rows[b];
   rows[b] = temp;
 
-  //string[] op = new string[rows.Length];
-
-  //for(int row = 0; row < rows.Length; row++)
-  //{
-  //    if (row != a && row != b) op[row] = "\\;";
-  //    if (row == a) op[row] = "R" + b.ToString();
-  //    if (row == b) op[row] = "R" + a.ToString();
-  //}
-
-  //operations += StringifyMatrix(createOperation(op));
+  if (options.steps == false) return;
 
   operations[a] += options.switch
     ? "R_{" + (a + 1) + "}\\leftrightarrow R_{" + (b + 1) + "}"
@@ -368,22 +435,14 @@ function multiplyRow(index, scalar) {
     rows[index][i] = math.simplify("(" + scalar + ")*(" + rows[index][i] + ")");
   }
 
-  //string[] op = new string[rows.Length];
-
-  //for(int row = 0; row < rows.Length; row++)
-  //{
-  //    if (row != index) op[row] = "\\;";
-  //    else { op[row] = " \\cdot " + scalar.ToString(); }
-  //}
-
-  //operations += StringifyMatrix(createOperation(op));
-
   let para;
   try {
     para = math.evaluate(scalar + " < 0");
   } catch (e) {
     para = false;
   }
+
+  if (options.steps == false) return;
 
   operations[index] +=
     " \\cdot " +
@@ -430,8 +489,15 @@ function addRow(addTo, addFrom, addFromScalar) {
 
   let numericOne;
 
+  let complex = false;
+
   try {
-    numericOne = !math.evaluate("abs(" + addFromScalar + ") == 1");
+    complex = math.evaluate("im(" + addFromScalar + ") != 0");
+  } catch (e) {}
+
+  try {
+    numericOne = math.evaluate("abs(" + addFromScalar + ") == 1") && !complex;
+    console.log(numericOne);
   } catch (e) {
     numericOne = false;
   }
@@ -444,10 +510,14 @@ function addRow(addTo, addFrom, addFromScalar) {
     cantEvaluate = true;
   }
 
+  if (options.steps == false) return;
+
   operations[addTo] +=
     (lessThanZero ? "-" : "+") +
-    (numericOne
-      ? math.simplify("abs(" + addFromScalar + ")").toTex() + "\\cdot "
+    (!numericOne && !cantEvaluate
+      ? (complex
+          ? "(" + math.simplify("" + addFromScalar).toTex() + ")"
+          : math.simplify("abs(" + addFromScalar + ")").toTex()) + "\\cdot "
       : "") +
     (cantEvaluate
       ? "(" + math.simplify(addFromScalar).toTex() + ") \\cdot "
@@ -495,7 +565,8 @@ function StringifyMatrix(matrix) {
   return result;
 }
 
-function SaveStatus() {
+function SaveStatus(inbetween = false) {
+  if (inbetween) result += `\\\\[${options.spacing}] \n ${options.equalSign} &`;
   result += StringifyMatrix(rows);
 }
 
@@ -503,8 +574,7 @@ function CompleteOperations() {
   if (!isNewOperations()) return;
   result += "\\!\\!\\! " + StringifyOperation(createOperation(operations));
   fillOperations();
-  result += `\\\\[${options.spacing}] \n \\sim &`;
-  SaveStatus();
+  SaveStatus(true);
 }
 
 function fillOperations() {
@@ -590,6 +660,7 @@ function draw() {
   for (var i = 0; i < drops.length; i++) {
     var text = letters[Math.floor(Math.random() * letters.length)];
     ctx.fillStyle = "#0f0";
+
     ctx.fillText(text, i * fontSize, drops[i] * fontSize);
     drops[i]++;
     if (drops[i] * fontSize > canvas.height && Math.random() > 0.95) {
@@ -600,3 +671,40 @@ function draw() {
 
 // Loop the animation
 setInterval(draw, 33);
+
+let availableOptionsText = `
+\\text{To use advanced options, type opt on the last line of your input} 
+\\\\
+\\text{followed by the options you want (seperated with spaces)}
+\\\\
+\\text{Like this:}
+\\\\\\\\
+\\begin{array}{c} \\text{1 2 3} \\\\[6pt]\\text{4 5 6} \\\\[6pt] \\text{opt inverse switch pmatrix} \\end{array}
+\\\\\\\\
+\\text{Available Options:}
+\\\\\\\\
+\\begin{align*}  
+\\begin{array}{|c|c|c|c|}  
+\\hline
+\\text{Option} & \\text{Description} & \\text{Example Usage} & \\text{How it looks} \\\\\\hline\\\\
+\\text{|} & \\text{Adds vertical line to matrix (must be on first row!)} & \\begin{array}{c} \\text{1 | 2} \\\\ \\text{3 4} \\end{array} & \\left[ \\begin{array}{c|c} 1 & 2 \\\\[6pt]3 & 4 \\end{array} \\right] \\\\\\hline\\\\
+\\text{bmatrix} & \\text{Uses square brackets for matrices (default)} & \\text{opt bmatrix} & \\left[\\begin{array}{cc} 1 & 2 \\\\[6pt]3 & 4 \\end{array}\\right] \\\\\\hline\\\\
+\\text{Bmatrix} & \\text{Uses square brackets for matrices} & \\text{opt Bmatrix} & \\left\\{\\begin{array}{cc} 1 & 2 \\\\[6pt]3 & 4 \\end{array}\\right\\} \\\\\\hline\\\\
+\\text{pmatrix} & \\text{Uses normal brackets for matrices} & \\text{opt pmatrix} & \\left(\\begin{array}{cc} 1 & 2 \\\\[6pt]3 & 4 \\end{array}\\right) \\\\\\hline\\\\
+\\text{vmatrix} & \\text{Uses line brackets for matrices} & \\text{opt vmatrix} & \\left|\\begin{array}{cc} 1 & 2 \\\\[6pt]3 & 4 \\end{array}\\right| \\\\\\hline\\\\
+\\text{Vmatrix} & \\text{Uses line brackets for matrices} & \\text{opt Vmatrix} & \\left|\\left|\\begin{array}{cc} 1 & 2 \\\\[6pt]3 & 4 \\end{array}\\right|\\right| \\\\\\hline\\\\
+\\text{matrix} & \\text{Uses no brackets for matrices} & \\text{opt matrix} & \\begin{array}{cc} 1 & 2 \\\\[6pt]3 & 4 \\end{array} \\\\\\hline\\\\
+\\text{noswitch} & \\text{Uses equal signs for switching rows (default)} & \\text{opt noswitch} & \\begin{array}{c} \\;=R_{2} \\\\[6pt]\\;=R_{1} \\end{array} \\\\\\hline\\\\
+\\text{switch} & \\text{Uses arrows for switching rows} & \\text{opt switch} & \\begin{array}{c} \\;R_{1}\\leftrightarrow R_{2} \\\\[6pt]\\;R_{2}\\leftrightarrow R_{1} \\end{array} \\\\\\hline\\\\
+\\text{equal} & \\text{Sets equal sign between matrices} & \\text{opt equal=}\\backslash \\text{to} & \\to \\left[ \\begin{array}{cc} 1 & 2 \\\\[6pt]3 & 2 \\end{array} \\right] \\\\\\hline\\\\
+\\text{zero} & \\text{Disables division with variables} & \\text{opt zero} & \\text{N/A} \\\\\\hline\\\\
+\\text{nonzero} & \\text{Enables division with variables (default)} & \\text{opt nonzero} & \\text{N/A} \\\\\\hline\\\\
+\\text{vspace} & \\text{Add specified spaces between lines} & \\text{opt vspace=6pt} & \\text{N/A} \\\\\\hline\\\\
+\\text{inverse} & \\text{Adds identity matrix to input, to calculate inverse} & \\text{opt inverse} & \\text{N/A} \\\\\\hline\\\\
+\\text{nosteps} & \\text{Hides steps and just shows start and result} & \\text{opt nosteps} & \\text{N/A} \\\\\\hline\\\\
+\\text{steps} & \\text{Shows steps (default)} & \\text{opt steps} & \\text{N/A} \\\\\\hline\\\\
+\\text{plain} & \\text{Just shows input and does not calculate anything} & \\text{opt plain} & \\text{N/A} \\\\\\hline\\\\
+\\text{result} & \\text{Just shows result} & \\text{opt result} & \\text{N/A} \\\\\\hline
+\\end{array}
+\\end{align*}%gauss.notrasmus.com/?opt
+`;
